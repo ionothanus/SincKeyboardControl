69ac92a63acae65aa63e111c2d24790c1586ac8a
Jonathan Moscardini <jonathan@appscardini.ca>
2021-07-07 00:48:24 -0400

    Implement polling for device events, system tray icon, autoconnect/refresh on start

--- SincHidController/SincHidController.cs
+++ SincHidController/SincHidController.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Device.Net;
 using Hid.Net;
@@ -11,19 +12,19 @@ namespace SincKeyboardControl.SincHid
 {
     internal static class JMStrings
     {
-        public static readonly string REQUEST_SELECT_WINDOWS = "\x00\x02JMLS0";
-        public static readonly string REQUEST_SELECT_MAC = "\x00\x02JMLS1";
-        public static readonly string REQUEST_LAYER_STATUS = "\x00\x02JMLR";
-        public static readonly string RESPONSE_MAC = "\x00\x02JML\x0e";
-        public static readonly string RESPONSE_WINDOWS = "\x00\x02JML\x0f";
-        public static readonly string EVENT_LAYER_WINDOWS = "JML0";
-        public static readonly string EVENT_LAYER_MAC = "JML1";
+        public static readonly string REQUEST_SELECT_WINDOWS = "\x00\x02JMLS0"; // transmit to kb: request layer 0 (Windows)
+        public static readonly string REQUEST_SELECT_MAC = "\x00\x02JMLS1"; // xmit to kb: request layer 1 (Mac)
+        public static readonly string REQUEST_LAYER_STATUS = "\x00\x02JMLR"; // xmit to kb: report current layer
+        public static readonly string RESPONSE_WINDOWS = "\x00\x02JML\x0f"; // recv from kb: response to layer request - Windows selected
+        public static readonly string RESPONSE_MAC = "\x00\x02JML\x0e"; // recv from kb: response to layer request - Mac selected
+        public static readonly string EVENT_LAYER_WINDOWS = "\x00\x02JML0"; // recv from kb: user changed layer using keyboard - Windows
+        public static readonly string EVENT_LAYER_MAC = "\x00\x02JML1"; // recv from kb: user changed layer using keyboard - Mac
         public static readonly int SHORTEST_STRING = 4;
     }
 
-    // TODO: 
-    // - wrap all comms in polling task, splitting write & read, so we can monitor for device events?
-    // - once done, implement event for layer change
+    // TODO:
+    // - separate events, rather than abusing PropertyChanged?
+    // - monitor connection state?
 
     public class SincHidController : IDisposable, INotifyPropertyChanged
     {
@@ -31,8 +32,11 @@ namespace SincKeyboardControl.SincHid
         private const int PID = 0x1267;
         private const ushort USAGE_PAGE = 0xFF60;
         private const int USAGE = 0x61;
+        private const int POLLING_INTERVAL = 500;
 
         private HidDevice device;
+
+        private bool polling;
 
         private SincLayerState lastState;
         public SincLayerState LastState
@@ -91,12 +95,45 @@ namespace SincKeyboardControl.SincHid
             return false;
         }
 
+        public Task StartPolling(CancellationToken ct)
+        {
+            if (!polling)
+            {
+                polling = true;
+
+                return Task.Run(async () =>
+                {
+                    ct.ThrowIfCancellationRequested();
+
+                    while (!ct.IsCancellationRequested && !disposed)
+                    {
+                        if (driverConnected)
+                        {
+                            var result = await device.ReadAsync(ct);
+
+                            if (result.BytesTransferred > 0)
+                            {
+                                LastState = ParseLayerState(result.Data);
+                            }
+                        }
+
+                        await Task.Delay(POLLING_INTERVAL);
+                    }
+
+                    polling = false;
+                });
+            }
+
+            return null;
+        }
+
         public void CloseDevice()
         {
             device.Close();
             DriverConnected = false;
         }
 
+        // Transmits data and immediately waits for a response. Used in "one-shot" functions.
         private async Task<TransferResult> SendRequestAndRetrieveResponseAsync(string request)
         {
             byte[] data = Encoding.ASCII.GetBytes(request);
@@ -106,8 +143,46 @@ namespace SincKeyboardControl.SincHid
             return await device.WriteAndReadAsync(realData).ConfigureAwait(false);
         }
 
-        public async Task<bool> UpdateLayerStatus()
+        // Merely transmits data - used in "polling" mode as the polling Task will read the response
+        private async Task<uint> SendRequestAsync(string request)
         {
+            byte[] data = Encoding.ASCII.GetBytes(request);
+            byte[] realData = new byte[65];
+            Array.Copy(data, realData, data.Length);
+
+            return await device.WriteAsync(realData).ConfigureAwait(false);
+        }
+
+        /// <summary>
+        /// If connected, sends a request for the layer status. Waits on the polling task to capture
+        /// the response & update the state.
+        /// </summary>
+        /// <returns>(through Task) true if successful, false if error occurred</returns>
+        public async Task<bool> UpdateLayerStatusPolling()
+        {
+            if (driverConnected)
+            {
+                uint result = await SendRequestAsync(JMStrings.REQUEST_LAYER_STATUS);
+
+                return result > 0;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// If connected, sends a request for the layer status. Waits on the polling task to capture
+        /// the response & update the state.
+        /// </summary>
+        /// <returns>(through Task) true if successful, false if error occurred</returns>
+        /// <exception cref="NotSupportedException">If polling mode is active, as the read will conflict with the polling reads. Use <seealso cref="UpdateLayerStatusPolling"/> if polling is enabled.</exception>
+        public async Task<bool> UpdateLayerStatusOneshot()
+        {
+            if (polling)
+            {
+                throw new NotSupportedException("Cannot use one-shot methods when polling is active");
+            }
+
             if (driverConnected)
             {
                 var result = await SendRequestAndRetrieveResponseAsync(JMStrings.REQUEST_LAYER_STATUS);
@@ -124,8 +199,40 @@ namespace SincKeyboardControl.SincHid
             return false;
         }
 
-        public async Task<bool> RequestLayer(SincLayerState layer)
+        public async Task<bool> RequestLayerPolling(SincLayerState layer)
         {
+            if (driverConnected)
+            {
+                string request;
+
+                switch (layer)
+                {
+                    case SincLayerState.Mac:
+                        request = JMStrings.REQUEST_SELECT_MAC;
+                        break;
+                    case SincLayerState.Windows:
+                        request = JMStrings.REQUEST_SELECT_WINDOWS;
+                        break;
+                    default:
+                        throw new NotImplementedException($"This controller doesn't know how to request the state {layer}");
+
+                }
+
+                uint result = await SendRequestAsync(request);
+
+                return result > 0;
+            }
+
+            return false;
+        }
+
+        public async Task<bool> RequestLayerOneshot(SincLayerState layer)
+        {
+            if (polling)
+            {
+                throw new NotSupportedException("Cannot use one-shot methods when polling is active");
+            }
+
             if (driverConnected)
             {
                 string request;
--- SincKeyboardControl/MainWindow.xaml.cs
+++ SincKeyboardControl/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -15,10 +17,10 @@ using System.Windows.Shapes;
 using SincKeyboardControl.SincHid;
 
 // TODO: 
-// - system tray icon?
-// - autoconnect on start?
+// - minimize to system tray icon
+// - change tray icon to reflect keyboard state?
+// - show keyboard state in tooltip?
 // - better error handling?
-// - once implemented in controller - status notifications when layer changed on keyboard?
 
 namespace SincKeyboardControl
 {
@@ -28,32 +30,110 @@ namespace SincKeyboardControl
     public partial class MainWindow : Window
     {
         private SincHidController controller;
+        private CancellationTokenSource cts = new CancellationTokenSource();
+        // private System.Drawing.Icon icon;
 
         public MainWindow()
         {
             controller = new SincHidController();
+
             Initialized += OnInit;
+            Closing += OnClosing;
             InitializeComponent();
         }
 
-        private void OnInit(object sender, EventArgs e)
+        private void OnClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            cts.Cancel();
+            taskbarIcon.Dispose();
+        }
+
+        private void ShowError(string text)
+        {
+            taskbarIcon.ShowBalloonTip("Error", text, Hardcodet.Wpf.TaskbarNotification.BalloonIcon.Warning);
+        }
+
+        private async void OnInit(object sender, EventArgs e)
         {
             lblCurrentState.DataContext = controller;
             lblConnected.DataContext = controller;
+            
+            // this successfully loads the icon, it just isn't good enough for ShowBalloonTip it seems
+            // var stream = Application.GetResourceStream(new Uri("pack://application:,,,/option.ico")).Stream;
+            // icon = new Icon(stream);
+
+            controller.PropertyChanged += Controller_PropertyChanged;
+            if (!await Connect())
+            {
+                ShowError("Unable to connect to the device");
+            }
+            else
+            {
+                if (!await RequestRefresh())
+                {
+                    ShowError("Unable to request a refresh from the device");
+                }
+            }
+
+            // do last - current implementation is that this returns the polling Task which waits indefinitely
+            await controller.StartPolling(cts.Token);
+        }
+
+        private void Controller_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(controller.DriverConnected):
+                    if (controller.DriverConnected)
+                    {
+                        taskbarIcon.ShowBalloonTip("Connected", "Connected to keyboard", Hardcodet.Wpf.TaskbarNotification.BalloonIcon.None);
+                    }
+                    else
+                    {
+                        taskbarIcon.ShowBalloonTip("Disconnected", "Disconnected from keyboard", Hardcodet.Wpf.TaskbarNotification.BalloonIcon.Warning);
+                    }
+                    break;
+                case nameof(controller.LastState):
+                    taskbarIcon.ShowBalloonTip($"{controller.LastState}", $"Keyboard is in {controller.LastState} mode", Hardcodet.Wpf.TaskbarNotification.BalloonIcon.None);
+                    break;
+                default:
+                    MessageBox.Show("Unknown element");
+                    break;
+            }
+
+        }
+
+        private async Task<bool> Connect()
+        {
+            return await controller.OpenDevice();
+        }
+
+        private async Task<bool> RequestRefresh()
+        {
+            return await controller.UpdateLayerStatusPolling();
+        }
+
+        private async Task<bool> RequestLayer(SincLayerState layerState)
+        {
+            return await controller.RequestLayerPolling(layerState);
         }
 
         private async void btnRequestLayer_Click(object sender, RoutedEventArgs e)
         {
-            var test = comboBox.SelectedItem;
-            if (!await controller.RequestLayer((SincLayerState)test))
+            var selectedItem = (SincLayerState)(comboBox.SelectedItem ?? SincLayerState.Unknown);
+
+            if (selectedItem != SincLayerState.Unknown)
             {
-                MessageBox.Show("Failed to refresh status");
+                if (!await RequestLayer(selectedItem))
+                {
+                    MessageBox.Show("Failed to refresh status");
+                }
             }
         }
 
         private async void btnRequestRefresh_Click(object sender, RoutedEventArgs e)
         {
-            if (!await controller.UpdateLayerStatus())
+            if (!await RequestRefresh())
             {
                 MessageBox.Show("Failed to refresh status");
             }
@@ -61,7 +141,7 @@ namespace SincKeyboardControl
 
         private async void btnConnect_Click(object sender, RoutedEventArgs e)
         {
-            if (!await controller.OpenDevice())
+            if (!await Connect())
             {
                 MessageBox.Show("Failed to open device");
             }
